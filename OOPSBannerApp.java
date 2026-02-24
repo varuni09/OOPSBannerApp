fe1b7f1871a825866d3a7dc71917084fd5c74b47
Varuni dekate <dekatevaruni3@gmail.com>
2026-02-24 13:38:29 +0530

    feat:addUC6

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,18 +1,50 @@
 public class OOPSBannerApp {
-
-    public static void main(String[] args) {
-        String[] lines = {
-
-            String.join(" ", " *** ", " *** ", " ***** ", " ***** "),
-            String.join(" ", "**  **", "**  **", "**  **", "**    "),
-            String.join(" ", "**  **", "**  **", "***** ", "***** "),
-            String.join(" ", "**  **", "**  **", "**     ", "    **"),
-            String.join(" ", "**  **", "**  **", "**     ", "**  **"),
-            String.join(" ", " *** ", " *** ", "**     ", " ***** ")
-
+    public static String[] getOPattern() {
+        return new String[] {
+            " *** ",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            " *** "
         };
-        for (String line : lines) {
-            System.out.println(line);
+    }
+
+    public static String[] getPPattern() {
+        return new String[] {
+            "**** ",
+            "*   *",
+            "*   *",
+            "**** ",
+            "*    ",
+            "*    ",
+            "*    "
+        };
+    }
+
+    public static String[] getSPattern() {
+        return new String[] {
+            " ****",
+            "*    ",
+            "*    ",
+            " *** ",
+            "    *",
+            "    *",
+            "**** "
+        };
+    }
+    public static void main(String[] args) {
+
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(oPattern[i] + "  " +
+                               oPattern[i] + "  " +
+                               pPattern[i] + "  " +
+                               sPattern[i]);
         }
     }
 }
